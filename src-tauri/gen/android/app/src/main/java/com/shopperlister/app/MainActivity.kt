50707cf053f15d0a3a6ccf1c0626c283c5b76567
EchoDev <anarchytrex@hotmail.com>
2025-06-27 18:58:10 +0200

    swapped to base vue due to complications

---
+++ src-tauri/gen/android/app/src/main/java/com/shopperlister/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.shopperlister.app
+
+class MainActivity : TauriActivity()
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>ShopperLister</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+
+a {
+  font-weight: 500;
+  color: #FFA500;
+  text-decoration: inherit;
+}
+
+h1 {
+  text-align: center;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    color: #f6f6f6;
+    background-color: #2f2f2f;
+  }
+
+  a:hover {
+    color: #24c8db;
+  }
+
+  input,
+  button {
+    color: #ffffff;
+    background-color: #0f0f0f98;
+  }
+
+  button:active {
+    background-color: #0f0f0f69;
+  }
+}
+</style>
