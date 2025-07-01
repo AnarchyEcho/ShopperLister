9848f8c9033d5d2a846778f3c1d201c7716c01e5
EchoDev <anarchytrex@hotmail.com>
2025-07-02 00:06:45 +0200

    removed unnecessary titles from pages

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1>Home</h1>
+  <div></div>
 </template>
 
 <style scoped lang="scss"></style>
--- src/pages/lists.vue
+++ src/pages/lists.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1>Lists</h1>
+  <div></div>
 </template>
 
 <style scoped lang="scss"></style>
